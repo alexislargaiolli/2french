c90403c4598b63492b9969c9d8f90517102ed554
alargaiolli <alexis.largaiolli@gmail.com>
2015-03-17 08:53:55 +0100

    .gitignore is now working

--- .tmp/public/scripts/controllers/translate.js
+++
@@ -1,9 +0,0 @@
-angular.module('tooFrenchApp').controller('TranslateController', function($translate, $scope) {
-  $scope.changeLanguage = function (langKey) {
-    $translate.use(langKey);
-  };
-
-  $scope.currentLanguage = function(){
-  	return $translate.use();
-  }
-});
--- .tmp/public/scripts/services/profile.js
+++
@@ -1,1 +0,0 @@
-var tooFrenchServices = angular.module('tooFrenchApp');
