727ffa196fbbda1608e32c80fb8fc8768fece152
alargaiolli <alexis.largaiolli@gmail.com>
2015-09-27 11:34:28 +0200

    Fix vendors minification

--- tasks/config/bowercopy.js
+++ tasks/config/bowercopy.js
@@ -7,25 +7,49 @@
  * ---------------------------------------------------------------
  *
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.config.set('bowercopy', {
         jsLibs: {
             options: {
-                runBower : false,
+                runBower: false,
                 destPrefix: '.tmp/public/vendors'
             },
             files: {
-                '2.cloudinary.js' : 'cloudinary_js/js/jquery.cloudinary.js',
-                '5.angular-translate.js' : 'angular-translate/angular-translate.js',
-                '6.angular-translate-files.js' : 'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
-                '7.angular-animate.js' : 'angular-animate/angular-animate.js',
-                '8.angular-coockie.js' : 'angular-cookies/angular-cookies.js',
-                '9.angular-resource.js' : 'angular-resource/angular-resource.js',
-                '10.angular-route.js' : 'angular-route/angular-route.js',
-                '11.angular-sanitize.js' : 'angular-sanitize/angular-sanitize.js',
-                '12.angular-touch.js' : 'angular-touch/angular-touch.js',
-                '13.angular-ui-router.js' : 'angular-ui-router/release/angular-ui-router.js'
+                '01.js': 'jquery/dist/jquery.js',
+                '02.js': 'angular/angular.js',
+                '1.angular-translate.js': 'angular-translate/angular-translate.js',
+                '2.angular-translate-files.js': 'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+                '3.angular-animate.js': 'angular-animate/angular-animate.js',
+                '4.angular-coockie.js': 'angular-cookies/angular-cookies.js',
+                '5.angular-resource.js': 'angular-resource/angular-resource.js',
+                '6.angular-route.js': 'angular-route/angular-route.js',
+                '7.angular-sanitize.js': 'angular-sanitize/angular-sanitize.js',
+                '8.angular-touch.js': 'angular-touch/angular-touch.js',
+                '9.angular-ui-router.js': 'angular-ui-router/release/angular-ui-router.js',
+
+                '10.js': 'bootstrap/dist/js/bootstrap.js',
+                '11.js': 'bootstrap-tour/build/js/bootstrap-tour.js',
+                '12.cloudinary.js': 'cloudinary_js/js/jquery.cloudinary.js',
+
+
+                '14.js': 'angular-xeditable/dist/js/xeditable.js',
+                '15.js': 'lodash/lodash.js',
+                '16.js': 'angular-google-maps/dist/angular-google-maps.js',
+                '17.js': 'angular-google-places-autocomplete/src/autocomplete.js',
+                '18.js': 'angular-ui-select/dist/select.js',
+                '19.js': 'angular-bootstrap/ui-bootstrap-tpls.js',
+                '21.js': 'ng-img-crop/compile/unminified/ng-img-crop.js',
+                '22.js': 'ng-file-upload/angular-file-upload-shim.js',
+                '23.js': 'cloudinary_ng/js/angular.cloudinary.js',
+                '24.js': 'ng-file-upload/angular-file-upload.js',
+                '25.js': 'moment/moment.js',
+                '26.js': 'moment/locale/fr.js',
+                '27.js': 'v-accordion/dist/v-accordion.js',
+                '28.js': 'angular-ui-router-anim-in-out/anim-in-out.js',
+                '29.js': 'angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.js',
+                '30.js': 'angular-carousel/dist/angular-carousel.js',
+                '31.js': 'angular-dialog-service/dist/dialogs.js',
             }
         }
     });
