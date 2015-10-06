ab3c30da290fccad5a5951c00d0233629fbe2512
alargaiolli <alexis.largaiolli@gmail.com>
2015-10-06 19:58:50 +0200

    update home carousel posters

--- assets/js/controllers/home.js
+++ assets/js/controllers/home.js
@@ -10,11 +10,13 @@ var ctrl = angular.module('tooFrenchCtrl');
 ctrl.controller('HomeCtrl', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
     $scope.interval = 500;
     var slides = $scope.slides = [{
-        image: 'http://res.cloudinary.com/hmn3vaygs/image/upload/v1440947478/diapo1_q7oakl.jpg'
+        image: 'https://res.cloudinary.com/hmn3vaygs/image/upload/v1444153962/diapo0_ux9jrb.jpg'
+    },{
+        image: 'https://res.cloudinary.com/hmn3vaygs/image/upload/v1440947478/diapo1_q7oakl.jpg'
     }, {
-        image: 'http://res.cloudinary.com/hmn3vaygs/image/upload/v1440947477/diapo2_eb2ckf.jpg'
+        image: 'https://res.cloudinary.com/hmn3vaygs/image/upload/v1444153972/diapo2_1_xfi6i3.jpg'
     }, {
-        image: 'http://res.cloudinary.com/hmn3vaygs/image/upload/v1440947479/diapo3_atnrof.jpg'
+        image: 'https://res.cloudinary.com/hmn3vaygs/image/upload/v1440947479/diapo3_atnrof.jpg'
     }];
 
     angular.element(document).ready(function () {
