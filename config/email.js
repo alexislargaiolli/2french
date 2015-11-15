62b5a1a8d3a629b00fbbe1b97d186982a1ce8da4
alargaiolli <alexis.largaiolli@gmail.com>
2015-11-15 16:06:40 +0100

    config mal pour la prod

--- config/email.js
+++ config/email.js
@@ -9,7 +9,7 @@ module.exports.email = {
         pass : '3cf353ad-1e6b-4fb9-b6bb-cd9168af4f30'
     },
     from : 'notification@toofrench.net',
-    templateDir : 'views/emailTemplates',
+    templateDir : 'views/emailTemplates'
     //testMode : true,
-    alwaysSendTo : 'alexis.largaiolli@gmail.com'
+    //alwaysSendTo : 'alexis.largaiolli@gmail.com'
 };
