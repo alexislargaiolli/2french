9bd12774ccf7ae7e441161d49331cb08ecb6e0e1
alex <alexis.largaiolli@gmail.com>
2016-02-27 09:33:35 +0100

    Stop du pooling de notif a la deconexion

--- assets/js/controllers/notification.js
+++ assets/js/controllers/notification.js
@@ -1,45 +1,91 @@
+/**
+ * Controller that manage notifications
+ */
 var tooFrenchControllers = angular.module('tooFrenchApp');
-tooFrenchControllers.controller('NotificationCtrl', ['$scope', 'Notification', '$rootScope', '$state', '$interval',
+tooFrenchControllers.controller('NotificationCtrl', ['$scope', '$rootScope', 'Notification', '$rootScope', '$state', '$interval', 'AUTH_EVENTS',
 
-    function ($scope, Notification, $rootScope, $state, $interval) {
+    function ($scope, $rootScope, Notification, $rootScope, $state, $interval, AUTH_EVENTS) {
 
+        /**
+         * Array of loaded notification
+         * @type {Array}
+         */
         $scope.notifications = [];
 
+        /**
+         * Number of unseen notification
+         * @type {number}
+         */
         $scope.notifCount = 0;
 
+        /**
+         * To if notification are being loading
+         * @type {boolean}
+         */
         $scope.notificationLoading = false;
 
+        /**
+         * Number of notif to skip for lazy loading
+         * @type {number}
+         */
         $scope.skip = 0;
+
+        /**
+         * Max number of notification for lazy loading
+         * @type {number}
+         */
         $scope.limit = 5;
 
+        /**
+         * Pointer on angular interval use to stop it
+         */
+        var stop;
+
+        /**
+         * Update notification count by asking to server
+         */
         $scope.updateCount = function () {
             Notification.getResource().unseenCount(function (data) {
                 $scope.notifCount = data.count;
             });
         }
         $scope.updateCount();
-        $interval($scope.updateCount, 5000);
+        stop = $interval($scope.updateCount, 5000);
 
+        /**
+         * On log out, stop interval on updateCount
+         */
+        $rootScope.$on(AUTH_EVENTS.logoutSuccess, function (event, args) {
+            if (angular.isDefined(stop)) {
+                $interval.cancel(stop);
+                stop = undefined;
+            }
+        });
+
+        /**
+         * Load notifications based on skip on limit variables
+         */
         $scope.loadNotif = function () {
-            $scope.skip = 0;
-            $scope.limit = 5;
             $scope.notificationLoading = true;
             Notification.getResource().unseen({skip: $scope.skip, limit: $scope.limit}, function (notifs) {
-                $scope.notifications = notifs;
+                $scope.notifications = $scope.notifications.concat(notifs);
                 $scope.notificationLoading = false;
             });
         }
 
+        /**
+         * Load 5 notification more
+         */
         $scope.loadMore = function () {
             $scope.skip += 5;
             $scope.limit += 5;
-            $scope.notificationLoading = true;
-            Notification.getResource().unseen({skip: $scope.skip, limit: $scope.limit}, function (notifs) {
-                $scope.notificationLoading = false;
-                $scope.notifications = $scope.notifications.concat(notifs);
-            });
+            $scope.loadNotif();
         }
 
+        /**
+         * Handle click on notification
+         * @param notif clicked notification
+         */
         $scope.onNotifClick = function (notif) {
             if (notif.type == 'resa') {
                 $state.go('planning', {history: notif.history, resaId: notif.reservation});
